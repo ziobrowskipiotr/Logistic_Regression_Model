cc16ea8971bfda9fcacf3ad2c0546f12c1c4cb38
Piotrek <piotrziobrowski.work@gmail.com>
2024-10-20 21:54:49 +0200

    build

---
+++ build/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/piete/Desktop/My_Logistic_Regression_Project/build/_deps/googletest-src/googletest
+# Build directory: C:/Users/piete/Desktop/My_Logistic_Regression_Project/build/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
